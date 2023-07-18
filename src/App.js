d5071b737c98867e74b8cf03f6ba6050eeb71499
Amajya singh <amjaya@technqtechnologies.com>
2023-07-18 17:09:22 +0530

    todolist

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Todolist from './components/todolist';
+import { useState } from 'react';
 
 function App() {
+  const [todo,setTodo] = useState("");
+  const [todoItems, setTodoItems] = useState([])
+  const handleTodoChange = (event) => {
+    setTodo(event.target.value)
+  }
+  const handleAddTodo = () => {
+      console.log(todo);
+      const date = new Date()
+      const newTodo = {
+        id : date.getTime(),
+        todoItem : todo
+      }
+      setTodoItems([...todoItems, newTodo])
+      setTodo("")
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     <h1>Todolist</h1>
+     <input type="text" value={todo} placeholder = "Enter todo" onChange={handleTodoChange} />
+     <button onClick={handleAddTodo} >Add Todo</button>
+     <div id='todolist__contianer' >
+       {todoItems.map((todoItem) => <Todolist key={todoItem.id} setTodoItems = {setTodoItems} todoItems = {todoItems} item = {todoItem} /> )}
+     </div>
+   </div>
   );
 }
 
